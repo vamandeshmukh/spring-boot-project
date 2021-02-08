695234f78460f90803e516c2d8696bde51140879
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-02-08 09:59:25 +0530

    Commti
    Commit

--- src/test/java/com/deloitte/spring/boot/demo/AppTest.java
+++ src/test/java/com/deloitte/spring/boot/demo/AppTest.java
@@ -1,38 +1,11 @@
 package com.deloitte.spring.boot.demo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+@SpringBootTest
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
